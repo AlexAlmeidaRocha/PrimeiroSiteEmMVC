412d65cf48b461077cd081b77f2853edad2b3376
Alex Almeida Rocha <alexalmeidarocha@outlook.com.br>
2022-01-27 13:56:21 -0300

    Add project files.

---
+++ PrimeiroSiteEmMVC/Controllers/SobreController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrimeiroSiteEmMVC.Controllers
+{
+    public class SobreController : Controller
+    {
+        // GET: SobreController/Pessoal
+        public ActionResult Pessoal()
+        {
+            return View();
+        }
+
+        // GET: SobreController/Profissinal
+        public ActionResult Profissional()
+        {
+            return View();
+        }      
+        
+    }
+}
